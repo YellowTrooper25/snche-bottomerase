eb8f5f9c7600f99d21398509578a492740ad8468
Santiago Sánchez <90863950+YellowTrooper25@users.noreply.github.com>
2022-02-21 14:19:54 -0600

    First commit

---
+++ IdeaProjects/untitled/src/main/kotlin/intro/example.kt
@@ -0,0 +1,25 @@
+package intro
+
+class Customer
+class Contact(val id: Int, var email: String )
+
+Contact
+private id
+getId()
+
+fun main() {
+
+    //Variable Mutable
+    var orange = "Oragn"
+    //Value Inmutable
+    val green = "Green"
+
+    var neverNull: String = "Not null"
+    neverNull = null
+    //Null cant be a value of a non-null type String
+
+    var client = Customer()
+    var contact = Contact(id:1, email:"aga.com")
+
+
+}
